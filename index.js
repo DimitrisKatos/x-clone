2d43befa1f1b505a9dcb9b816aeedd064b84bf77
Dimitris_Katos <dimitriskatos6@gmail.com>
2026-03-04 20:15:49 +0200

    Adding color when post liked and see all the replies, starting the post new tweet methodology

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { tweetsData } from './data.js'
 /* CDN: Content Delivery Network*/
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+
  
 const tweetInput = document.getElementById('chat-input')
 const tweetBtn = document.getElementById('tweet-btn')
@@ -12,6 +14,7 @@ tweetBtn.addEventListener('click', function(){
 
 function getFeedHtml(){
     
+
     let feedHtml = ``
     // for(let tweet of tweetsData){
     //     feedHtml += `
@@ -39,6 +42,26 @@ function getFeedHtml(){
         
     // }
     tweetsData.forEach(function(tweet){
+        let feedReplyHtml = ``
+        if(tweet.replies.length > 0){
+            console.log(`The ${tweet.uuid} has a reply`)
+
+            
+            for(let reply of tweet.replies){
+                feedReplyHtml += `
+                    <div class="tweet-reply">
+                        <div class="tweet-inner">
+                            <img src="${reply.profilePic}" class="profile-pic">
+                                <div>
+                                    <p class="handle">${reply.handle}</p>
+                                    <p class="tweet-text">${reply.tweetText}</p>
+                                </div>
+                            </div>
+                    </div>`
+            }
+        }
+
+
         feedHtml += `
             <div class="tweet">
                 <div class="tweet-inner">
@@ -52,17 +75,23 @@ function getFeedHtml(){
                             <span class="tweet-detail">
                                 ${tweet.replies.length}
                             </span>
-                            <i class="fa-solid fa-heart" data-like="${tweet.uuid}"></i>
+                            <i class="fa-solid fa-heart ${tweet.isLiked ? 'liked' : ''}" data-like="${tweet.uuid}"></i>
                             <span class="tweet-detail">
                                 ${tweet.likes}
                             </span>
-                            <i class="fa-solid fa-retweet" data-retweet="${tweet.uuid}"></i>
+                            <i class="fa-solid fa-retweet ${tweet.isRetweeted ? 'retweeted' : ''}" 
+                                data-retweet="${tweet.uuid}">
+                            </i>
                             <span class="tweet-detail">
                                 ${tweet.retweets}
                             </span>
                         </div>   
                     </div>            
                 </div>
+                <div class="hidden" id="replies-${tweet.uuid}">
+                    ${feedReplyHtml}
+                </div> 
+
             </div>`
     })
 
@@ -80,16 +109,73 @@ render()
 document.addEventListener('click', function(e) {
     if(e.target.dataset.reply){
         console.log("A reply added "+e.target.dataset.reply)
-        handleLikeClick(e.target.dataset.reply)
+        handleReplyClick(e.target.dataset.reply)
     }
-    if(e.target.dataset.like){
-        console.log("A comment added "+e.target.dataset.like)
+    else if(e.target.dataset.like){
+        // console.log("A like added "+e.target.dataset.like)
+        handleLikeClick(e.target.dataset.like)
     }
-    if(e.target.dataset.retweet){
+    else if(e.target.dataset.retweet){
         console.log("A retwett made "+e.target.dataset.retweet)
+        handleRetweetClick(e.target.dataset.retweet)
+    }else if(e.target.id === "tweet-btn"){
+        handleTweetBtnClick()
     }
+
 })
 
+function handleTweetBtnClick(){
+    const tweetInput = document.getElementById("chat-input")
+    
+    tweetsData.push({
+        handle: `@JimDonw`,
+        profilePic: `images/musk.png`,
+        tweetText:`${tweetBtn.value}`,
+        uuid: uuidv4()
+    })
+    console.log("xyseme 2: " + JSON.stringify(tweetsData))
+    render()
+}
+
 function handleLikeClick(tweetId){
+
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
+
+    if(targetTweetObj.isLiked){
+        targetTweetObj.likes--
+    } else {
+        targetTweetObj.likes++
+    }
+
+    targetTweetObj.isLiked = !targetTweetObj.isLiked
+
+    render()
+}
+
+
+function handleRetweetClick(tweetId){
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
+
+    if(targetTweetObj.isRetweeted){
+        targetTweetObj.retweets--
+    } else {
+        targetTweetObj.retweets++
+    }
+
+    targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
+
+    render()
+}
+
+
+function handleReplyClick(tweetId){
     console.log(tweetId)
+
+    const replies = document.getElementById(`replies-${tweetId}`)
+    replies.classList.toggle('hidden')
+
 }
